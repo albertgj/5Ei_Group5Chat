4f37c0f2f148e578465c610f0986c5ecfe28715a
R3c0n4u <R3c0n4u@DESKTOP-67SALMR>
2019-11-16 21:34:39 +0100

    Implemented usertochat packet sender.Added usertochat Implementation

--- src/usertochat/Main.java
+++ src/usertochat/Main.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package usertouser;
+package usertochat;
 
 /**
  *
- * @author 17659
+ * @author Donzi
  */
 public class Main 
 {
